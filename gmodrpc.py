b03ba38c162023dcbbe615b826ec2579e4ad4b63
Alex Kalderimis <alex@flymine.org>
2011-10-14 20:03:21 +0100

    Added fulltext and location service

--- gmodrpc.py
+++ gmodrpc.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 from intermine.webservice import Service
+from interminebio import RegionQuery
+import suds.client
+import time
 
 class GMODAdapter(object):
 
@@ -13,8 +16,18 @@ class GMODAdapter(object):
         'metabolicmine': "http://metabolicmine.org/test",
         }
 
+    default_species = {
+        'flymine': "D. melanogaster",
+        'yeastmine': "S. cerevisiae",
+        'ratmine': "R. norvegicus",
+        'metabolicmine': "H. sapiens"
+        }
+
+    EBI = "http://www.ebi.ac.uk/ontology-lookup/OntologyQuery.wsdl"
+
     def __init__(self):
         self.services = dict(map(lambda x: (x[0], Service(x[1])), self.service_urls.items()))
+        self.ebi = suds.client.Client(self.EBI)
 
     def organisms(self, mine):
         service = self.services[mine]
@@ -22,5 +35,58 @@ class GMODAdapter(object):
         orgs = [{"organism": {"genus": row["genus"], "species": row["species"], "taxonomy_id": row["taxonId"]}} for row in query.rows()]
         result = {"data_provider": mine, "api_version": GMODAdapter.API_VERSION, "data_version": service.release, "result": orgs}
         return result
+
+    def search(self, mine, term, type="Gene", taxid=None, genus=None, species=None):
+        service = self.services[mine]
+        if type.startswith("SO:"):
+            type = self._resolve_so_id(type).capitalize()
+        query = service.new_query(type).select("*").where(type, "LOOKUP", term)
+        if taxid is not None:
+            query = query.where(type + ".organism.taxonId", "=", taxon)
+        else:
+            if genus is not None:
+                query = query.where(type + ".organism.genus", "=", genus)
+            if species is not None:
+                query = query.where(type + ".organisms.species", "=", species)
+
+        results = [dict(map(lambda x: (x[0].split('.')[-1], x[1]), row.to_d().items())) for row in query.rows()]
+        for r in results:
+            r["accession"] = r["primaryIdentifier"]
+        result = {"query_time": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "data_provider": mine, "api_version": GMODAdapter.API_VERSION, "data_version": service.release, "result": results}
+        return result
+
+    def location(self, mine, chromsome, strand=None, fmin=None, fmax=None, type=["SequenceFeature"], taxid=None, genus=None, species=None, organism=None):
+        service = self.services[mine]
+        if type[0].startswith("SO:"):
+            className = self._resolve_so_id(type[0]).capitalize()
+        else:
+            className = type[0]
+        query = service.new_query(className).select("primaryIdentifier").where("chromosome.primaryIdentifier", '=', chromsome)
+        if fmin is not None:
+            query = query.where("chromosomeLocation.start", '>=', fmin[0])
+        if fmax is not None:
+            query = query.where("chromosomeLocation.end", '>=', fmax[0])
+        if strand is not None:
+            query = query.where("chromosomeLocation.strand", '=', strand)
+        if taxid is not None:
+            query = query.where(type + ".organism.taxonId", "=", taxid)
+        else:
+            if genus is not None:
+                query = query.where(type + ".organism.genus", "=", genus)
+            if species is not None:
+                query = query.where(type + ".organisms.species", "=", species)
+            if species is None and genus is None:
+                organism = self.default_species[mine]
+
+        results = [{"accession": row["primaryIdentifier"]} for row in query.rows()]
+
+        result = {"query_time": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "data_provider": mine, "api_version": GMODAdapter.API_VERSION, "data_version": service.release, "result": results}
+        return result
+
+    def _resolve_so_id(self, so_id): 
+        return self.ebi.service.getTermById(so_id, "SO")
+
+
+
 
 
--- gmodrpc_app.py
+++ gmodrpc_app.py
@@ -10,10 +10,24 @@ app.secret_key = os.urandom(24)
 
 gmod_adaptor = GMODAdapter()
 
-@app.route('/<mine>/gmodrpc/1.1/organisms.json')
+@app.route('/<mine>/gmodrpc/v1.1/organisms.json')
 def organisms(mine):
     data = gmod_adaptor.organisms(mine)
     return jsonify(**data)
+
+@app.route('/<mine>/gmodrpc/v1.1/fulltext/<term>.json')
+def fulltext(mine, term):
+    args = request.args
+    results = gmod_adaptor.search(mine, term, **args)
+    results["query_url"] = request.url
+    return jsonify(**results)
+
+@app.route('/<mine>/gmodrpc/v1.1/location/<location>.json')
+def location(mine, location):
+    args = request.args
+    results = gmod_adaptor.location(mine, location, **args)
+    results["query_url"] = request.url
+    return jsonify(**results)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True, port=5005)
